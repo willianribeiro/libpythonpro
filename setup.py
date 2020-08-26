469c213eab5bf53c9b98ab07ece1a18db5ed88ff
Willian Ribeiro <willian.ribeiro@buser.com.br>
2020-08-25 22:12:09 -0300

    Altera nome do pacote para publicar no PyPi
    O nome libpythonpro já existe no PyPi, o que impede que eu publique com
    esse mesmo nome. Por essa razão alterei o nome do pacote para
    my_libpythonpro.

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "my_libpythonpro"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Willian Bittencourt Ribeiro"
