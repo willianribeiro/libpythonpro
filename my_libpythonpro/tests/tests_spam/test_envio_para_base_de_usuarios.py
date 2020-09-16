f23706b9e098d6a1b78645f2aced28b611556707
Willian Ribeiro <willian.ribeiro@buser.com.br>
2020-09-16 19:17:44 -0300

    Troca EnviadorMock por unittest.mock.Mock

--- my_libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
+++ my_libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
@@ -3,7 +3,6 @@ from unittest.mock import Mock
 import pytest
 
 from my_libpythonpro.spam.main import EnviadorDeSpam
-from my_libpythonpro.spam.enviador_de_email import Enviador
 from my_libpythonpro.spam.modelos import Usuario
 
 
@@ -22,40 +21,29 @@ from my_libpythonpro.spam.modelos import Usuario
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'remetente@email.com.br',
         'Assunto do e-mail',
         'Corpo do e-mail'
     )
-    assert len(sessao.listar()) == enviador.qtd_emails_enviados
+    assert len(sessao.listar()) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Usuario Um', email='usuario_um@email.com.br')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'remetente@email.com.br',
         'Assunto do e-mail',
         'Corpo do e-mail'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'remetente@email.com.br',
         'usuario_um@email.com.br',
         'Assunto do e-mail',
         'Corpo do e-mail'
     )
-
-
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.parametros_de_envio = None
-        self.qtd_emails_enviados = 0
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.qtd_emails_enviados += 1
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
