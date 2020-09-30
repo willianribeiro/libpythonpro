3f718a99a9e4ff66ca5c16f6b3093491f68e5d63
Willian Ribeiro <willian.ribeiro@buser.com.br>
2020-09-30 19:18:38 -0300

    Adiciona tests par ao github_api.py

---
+++ my_libpythonpro/tests/test_github_api.py
@@ -0,0 +1,34 @@
+from unittest.mock import Mock
+
+import pytest
+
+from my_libpythonpro import github_api
+
+
+@pytest.fixture()
+def avatar_url():
+    # mock api
+    url = 'https://avatars1.githubusercontent.com/u/3102551?v=4'
+    json_mock = {
+        'login': 'willianribeiro',
+        'id': 3102551,
+        'node_id': 'MDQ6VXNlcjMxMDI1NTE=',
+        'avatar_url': url
+    }
+    response_mock = Mock()
+    response_mock.json.return_value = json_mock
+    get_original = github_api.requests.get
+    github_api.requests.get = Mock(return_value=response_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('willianribeiro')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renzon')
+    expected_url = 'https://avatars3.githubusercontent.com/u/3457115?v=4'
+    assert expected_url == url
